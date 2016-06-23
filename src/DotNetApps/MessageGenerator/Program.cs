d427a165c47d12f45ee607c282b83fe987030ff9
ashic <ashic@live.com>
2016-06-23 07:04:23 +0100

    added generator app

---
+++ src/DotNetApps/MessageGenerator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Kafka.Basic;
+
+namespace MessageGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random(DateTime.Now.Millisecond);
+
+            //TODO: Setup consumer...need topic (see below)
+            {
+                while (true)
+                {
+                    var batch = Enumerable.Range(1, 10).Select(x =>
+                        new Message()
+                        {
+                            Key = "a",
+                            Value = (DateTime.Now.Millisecond + random.Next()).ToString()
+                        }).ToArray();
+
+                    //TODO: Uncomment
+                    //topic.Send(batch);
+                    Console.WriteLine("Sending batch....");
+                   Thread.Sleep(2000);
+                }
+            }
+        }
+    }
+}
